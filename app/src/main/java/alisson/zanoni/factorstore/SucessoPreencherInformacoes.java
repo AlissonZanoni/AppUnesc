d578fbf94b17cea22db2a3f88c7636cd34a889dd
AlissonZanoni <alisson.zanonii@gmail.com>
2022-03-11 11:50:53 -0300

    Inserindo tela para cadastrar informações para automação

---
+++ app/src/main/java/alisson/zanoni/factorstore/SucessoPreencherInformacoes.java
@@ -0,0 +1,14 @@
+package alisson.zanoni.factorstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SucessoPreencherInformacoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucesso_preencher_informacoes);
+    }
+}
